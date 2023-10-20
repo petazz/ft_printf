b6dfa0fe6332fa530a9438c6ee683aab8e6cca6a
Pedro Gonzalez Cepeda <pgonzal2@c1r15s6.42malaga.com>
2023-10-20 20:03:59 +0200

    2

---
+++ srcs/ft_putnbr_HEX.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_HEX.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pgonzal2 <pgonzal2@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 10:36:09 by pgonzal2          #+#    #+#             */
+/*   Updated: 2023/10/20 16:42:34 by pgonzal2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putnbr_HEX(unsigned long nbr)
+{
+	if (nbr >= 16)
+	{
+		ft_putnbr_base((nbr / 16));
+		ft_putnbr_base(nbr % 16);
+	}
+	if (nbr <= 9)
+		ft_putchar((nbr % 10) + '0');
+	else if (nbr < 16 && nbr > 9)
+		ft_putchar(nbr - 10 + 'A');
+}
+
